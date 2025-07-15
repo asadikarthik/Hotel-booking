21136b8b0f031cf044a74aed1c4dc094bf9bbcc1
Karthik Asadi <155179825+asadikarthik@users.noreply.github.com>
2025-07-15 22:28:55 +0530

    Update Layout.jsx

--- client/src/pages/HotelOwner/Layout.jsx
+++ client/src/pages/HotelOwner/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
-import Navbar from '../../components/hotelOwner/Navbar'
-import Sidebar from '../../components/hotelOwner/Sidebar'
+import Navbar from '../../components/HotelOwner/Navbar'
+import Sidebar from '../../components/HotelOwner/Sidebar'
 import { Outlet } from 'react-router-dom'
 import { useAppContext } from '../../context/AppContext'
 
