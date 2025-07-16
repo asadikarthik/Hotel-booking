871d15aca9de7594b29d2cdcd68ea952621b6ab0
Karthik Asadi <asadikarthik17@gmail.com>
2025-07-16 20:14:09 +0530

    Fix: Correct hero image Tailwind path for Vercel

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
 
   return (
     <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 
-    xl:px-32 bg-[url("src/assets/heroImage2.jpg")] text-white bg-no-repeat bg-cover bg-center 
+    xl:px-32 bg-[url("/heroImage2.jpg")] text-white bg-no-repeat bg-cover bg-center 
     h-screen'>
 
     
